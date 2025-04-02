15cac3c2281d578affb1260793400fd358890e51
rastaclaus <tema.rastaclaus@gmail.com>
2025-04-02 12:23:11 +0300

    add typing extensions to python 3.9

--- src/config_lib/utils/types.py
+++ src/config_lib/utils/types.py
@@ -9,12 +9,18 @@ from typing import Any
 
 # Conditional import of TypeGuard
 if sys.version_info < (3, 10):
+    from typing import Union
+
     from typing_extensions import TypeGuard
 else:
     from typing import TypeGuard
 
 # Type aliases for configuration values
-ConfigScalar = str | int | float | datetime | date | None
+if sys.version_info < (3, 10):
+    ConfigScalar = Union[str, int, float, datetime, date, None]
+else:
+    ConfigScalar = str | int | float | datetime | date | None
+
 ConfigSequence = Sequence[ConfigScalar]
 # Recursive type definition using a forward reference.
 ConfigMapping = MutableMapping[str, "ConfigMapping | ConfigScalar | ConfigSequence"]
